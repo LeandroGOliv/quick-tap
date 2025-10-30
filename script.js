9d343c915d48f4b7ad41640ca83a923dd5d3ff43
Leandro <leandro.golub123@gmail.com>
2025-10-30 01:28:58 -0300

    feat: commit inicial com funcionalidade basica do jogo

---
+++ script.js
@@ -0,0 +1,42 @@
+const startButton = document.getElementById("start");
+const menu = document.getElementById("menu");
+const modeButton = document.getElementById("gameMode");
+modeButton.textContent = "Normal";
+
+modeButton.addEventListener("click", toggleMode);
+
+startButton.addEventListener("click", startGame);
+
+function toggleMode() {
+  if (modeButton.textContent === "Normal") {
+    modeButton.textContent = "Mini flick";
+    console.log("if");
+  } else {
+    console.log("else");
+    modeButton.textContent = "Normal";
+  }
+}
+
+function startGame() {
+  console.log("starting...");
+  const target = document.createElement("div");
+  target.id = "target";
+  applyTargetStyle(target);
+  moveTarget(target);
+  document.body.appendChild(target);
+  target.addEventListener("click", () => moveTarget(target));
+}
+
+function applyTargetStyle(target) {
+  if (modeButton.textContent === "Normal") {
+    target.className = "normalTarget";
+  } else {
+  }
+}
+
+function moveTarget(target) {
+  const x = Math.random() * (window.innerWidth - 400);
+  const y = Math.random() * (window.innerHeight - 200);
+  target.style.left = `${x}px`;
+  target.style.top = `${y}px`;
+}
