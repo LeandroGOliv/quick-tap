9f35527979e914d6cf92d664ce94a7ffdacc7081
Leandro <leandro.golub123@gmail.com>
2025-10-31 20:52:46 -0300

    wip: divide jogo em dois modos, com tempo e infinito

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 const startButton = document.getElementById("start");
 const menu = document.getElementById("menu");
 const modeButton = document.getElementById("gameMode");
+const timeModeButton = document.getElementById("timeMode");
 const countDisplay = document.getElementById("countDisplay");
 let isStarted = false;
 let count = 0;
 let timeoutId;
 
+timeModeButton.textContent = "Infinite";
 modeButton.textContent = "Normal";
 
-modeButton.addEventListener("click", toggleMode);
+timeModeButton.addEventListener("click", toggleTimeMode);
+modeButton.addEventListener("click", toggleGameMode);
 startButton.addEventListener("click", startGame);
 
-function toggleMode() {
+function toggleGameMode() {
   if (modeButton.textContent === "Normal") {
     modeButton.textContent = "Mini flick";
-    console.log("if");
   } else {
-    console.log("else");
     modeButton.textContent = "Normal";
+  }
+}
+function toggleTimeMode() {
+  if (timeModeButton.textContent === "Infinite") {
+    timeModeButton.textContent = "With time"; // TO DO, add time customization
+  } else {
+    timeModeButton.textContent = "Infinite";
   }
 }
 
 function startGame() {
+  countDisplay.style.visibility = "visible";
+  countDisplay.textContent = "Contador: 0";
+  const targetContainer = document.createElement("div");
+  targetContainer.id = "targetContainer";
   const target = document.createElement("div");
-  countDisplay.textContent = "Contador:";
   target.id = "target";
+  targetContainer.appendChild(target);
+  targetContainer.className = "targetContainer";
   applyTargetStyle(target);
-  moveTarget(target);
-  document.body.appendChild(target);
-  target.addEventListener("click", () => onTargetClick(target));
-  startTimeout(target);
+  moveTarget(targetContainer);
+  document.body.appendChild(targetContainer);
+  target.addEventListener("click", () => onTargetClick(targetContainer));
+  menu.style.display = "none";
+  startTimeout(targetContainer);
 }
 
 function applyTargetStyle(target) {
@@ -39,23 +53,51 @@ function applyTargetStyle(target) {
   }
 }
 
-function moveTarget(target) {
+function moveTarget(targetContainer) {
   const x = Math.random() * (window.innerWidth - 1000);
   const y = Math.random() * (window.innerHeight - 500);
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
+  targetContainer.style.left = `${x}px`;
+  targetContainer.style.top = `${y}px`;
 }
 
-function onTargetClick(target) {
+function onTargetClick(targetContainer) {
   clearTimeout(timeoutId);
-  moveTarget(target);
+  moveTarget(targetContainer);
   count++;
-  countDisplay.textContent = "Contador:" + count;
-  startTimeout(target);
+  countDisplay.textContent = "Contador: " + count;
 }
-function startTimeout(target) {
+function startTimeout(targetContainer) {
+  let duration;
+  if (timeModeButton.textContent === "With time") {
+    duration = 50000;
+    const timeBar = document.createElement("div");
+    timeBar.id = "timeBar";
+    document.body.appendChild(timeBar);
+    timeBar.className = "timeBar";
+    timeBar.style.animation = "none";
+    timeBar.offsetHeight;
+    timeBar.style.animation = `shrinkBar ${duration}ms linear forwards`;
+
+    timeoutId = setTimeout(() => {
+      targetContainer.remove();
+      alert(`Time is up!. \nYour score: ${count}`);
+      countDisplay.style.visibility = "hidden";
+      count = 0;
+      menu.style.display = "block";
+    }, duration);
+  } else {
+    duration = 2000;
+    const targetTimeBar = document.createElement("div");
+    targetTimeBar.id = "targetTimeBar";
+    targetContainer.appendChild(targetTimeBar);
+    targetTimeBar.className = "targetTimeBar";
+    targetTimeBar.style.animation = "none";
+    targetTimeBar.offsetHeight;
+    targetTimeBar.style.animation = `shrinkBar ${duration}ms linear forwards`;
+  }
   timeoutId = setTimeout(() => {
-    target.remove();
-    alert("defeat");
-  }, 800);
+    targetContainer.remove();
+    alert(`Defeat\nYour score: ${count}`);
+  }, duration);
 }
+// separar em mais funcoes
